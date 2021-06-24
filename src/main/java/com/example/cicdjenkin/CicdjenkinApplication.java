5c3a5b313a2a22236d998d9593ba7c32bc4c5410
bvhieu <buivinhhieu217@gmail.com>
2021-06-24 16:18:03 +0700

    first commit

---
+++ src/main/java/com/example/cicdjenkin/CicdjenkinApplication.java
@@ -0,0 +1,21 @@
+package com.example.cicdjenkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CicdjenkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdjenkinApplication.class, args);
+    }
+
+    @GetMapping("/ci")
+    public String Hello() {
+        return "Welcome to ci cd with Jenkins";
+    }
+
+}
---
+++ src/test/java/com/example/cicdjenkin/CicdjenkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdjenkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdjenkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
