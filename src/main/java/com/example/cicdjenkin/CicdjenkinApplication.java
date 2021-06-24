9083cf0f69317cddafeed9bc354ec6fce12ad4b6
Trungg <nguyentrung030696@gmail.com>
2021-06-24 22:49:31 +0700

    up

--- src/main/java/com/example/cicdjenkin/CicdjenkinApplication.java
+++ src/main/java/com/example/cicdjenkin/CicdjenkinApplication.java
@@ -15,7 +15,7 @@ public class CicdjenkinApplication {
 
     @GetMapping("/ci")
     public String Hello() {
-        return "Welcome to ci cd with Jenkins11!";
+        return "Welcome to ci cd with Jenkins1221!";
     }
 
 }
