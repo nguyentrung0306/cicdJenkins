704f2136b294b49c6cb3e2a6ff3d546f914130b7
Trungg <nguyentrung030696@gmail.com>
2021-06-25 10:28:46 +0700

    Update CicdjenkinApplication.java

--- src/main/java/com/example/cicdjenkin/CicdjenkinApplication.java
+++ src/main/java/com/example/cicdjenkin/CicdjenkinApplication.java
@@ -15,7 +15,7 @@ public class CicdjenkinApplication {
 
     @GetMapping("/ci")
     public String Hello() {
-        return "Welcome to ci cd with Jenkins test lan thu 8!";
+        return "Welcome to ci cd with Jenkins test lan thu 81!";
     }
 
 }
